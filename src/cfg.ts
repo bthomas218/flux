90f23f933d226cc5ffc7698f770e4ffe7662afd7
brady <bradythomas690@gmail.com>
2026-04-28 15:04:57 -0500

    feat: setup server configuration

---
+++ src/cfg.ts
@@ -0,0 +1,21 @@
+import { treeifyError, z } from "zod";
+import "dotenv/config";
+
+const envSchema = z.object({
+  NODE_ENV: z
+    .enum(["development", "production", "test"])
+    .default("development"),
+  PORT: z.coerce.number().default(3000),
+  HOSTNAME: z.string().min(1).default("localhost"),
+});
+
+const parsed = envSchema.safeParse(process.env);
+
+if (!parsed.success) {
+  console.error("Invalid enviroment variables");
+  const err = z.treeifyError(parsed.error);
+  console.error(err.properties);
+  process.exit(1);
+}
+
+export const cfg = parsed.data;
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import Fastify from "fastify";
+import { cfg } from "./cfg.js";
 
 const app = Fastify({
   logger: true,
@@ -10,7 +11,7 @@ app.get("/health", async (request, reply) => {
 
 async function main() {
   try {
-    await app.listen({ port: 3000 });
+    await app.listen({ port: cfg.PORT, host: cfg.HOSTNAME });
   } catch (err) {
     app.log.error(err);
     process.exit(1);
