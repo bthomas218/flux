d96665d8acdcec12b7d7ffc5bb4588c99ab6ddf1
brady <bradythomas690@gmail.com>
2026-04-28 14:50:08 -0500

    feat: set up fastify server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello TypeScript!");
+import Fastify from "fastify";
+
+const app = Fastify({
+  logger: true,
+});
+
+app.get("/health", async (request, reply) => {
+  return { status: "OK" };
+});
+
+async function main() {
+  try {
+    await app.listen({ port: 3000 });
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+}
+
+main();
